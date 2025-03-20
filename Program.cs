ca205ce68937353f5287ed8b9f380ad5bd9b13b9
Gabi-Nap <gabriellopez4195@gmail.com>
2025-03-20 14:53:49 -0300

    Parte1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Prueba");
-Console.WriteLine("Prueba");
+﻿using System;
+namespace helloworld
+{
+	class Program
+	{
+		static void Main(string[]args)
+		{
+            Auto auto1= new Auto();
+            auto1.color="negro";
+            auto1.marca="fiat";
+            auto1.pasajeros=2;
+            auto1.Encender();
+		}
+        public class Auto
+        {
+            public string color;
+            public string marca;
+            public int pasajeros;
+
+            public void Encender()
+            {
+                Console.WriteLine("El auto esta encendido");
+            }
+            public void AbrirPuerta()
+            {
+                Console.WriteLine("La puerta esta abrierta");
+            }
+        }
+	}
+}
