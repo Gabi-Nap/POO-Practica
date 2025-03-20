539dd334ec59205f6a20d6d47f5517c73ff45b59
Gabi-Nap <gabriellopez4195@gmail.com>
2025-03-20 15:17:51 -0300

    ProgramaTerminado

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace helloworld
             auto1.marca="fiat";
             auto1.pasajeros=2;
             auto1.Encender();
+
+            Restaurante rest1=new Restaurante();
+            rest1.cantMozos=10;
+            rest1.direccion="Sarmiento 700";
+            rest1.mesas= 20;
+            rest1.Cocinar();
 		}
         public class Auto
         {
@@ -24,6 +30,20 @@ namespace helloworld
             public void AbrirPuerta()
             {
                 Console.WriteLine("La puerta esta abrierta");
+            }
+        }
+        public class Restaurante
+        {
+            public int mesas;
+            public string direccion;
+            public int cantMozos;
+            public void Cocinar()
+            {
+                Console.WriteLine("Se esta empezando a cocinar");
+            }
+            public void Emplatar()
+            {
+                Console.WriteLine("Se esta emplatando el plato");
             }
         }
 	}
