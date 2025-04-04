d0b0044b9b7fbec05cbf6e27970b6ebc3f80f379
Gabi-Nap <gabriellopez4195@gmail.com>
2025-04-04 16:16:32 -0300

    PrimerEjercicioClase3

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Vehiculo v1= new Vehiculo();
+            v1.capacidad=30;
+            v1.cantPuertas=3;
+            v1.cantAsientos=20;
+            v1.Arrancar();
+            Autobus bus1=new Autobus();
+            bus1.empresa="dota";
+            bus1.CargaPasajeros();
+        }
+        public class Vehiculo
+        {
+            public int capacidad;
+            public int cantPuertas;
+            public int cantAsientos;
+            public void Arrancar()
+            {
+                Console.WriteLine("El vehiculo esta arrancando");
+            }
+            public void Detener()
+            {
+                Console.WriteLine("El vehiculo se esta deteniendo");
+            }
+            public void Frenar()
+            {
+                Console.WriteLine("El vehiculo esta frenando");
+            }
+            public void Acelerar()
+            {
+                Console.WriteLine("El vehiculo esta acelerando");
+            }
+
+        }
+        public class Autobus:Vehiculo
+        {
+            public string empresa;
+            public void CargaPasajeros()
+            {
+                Console.WriteLine("pasajeros ascendiendo");
+            }
+            public void DescargaPasajeros()
+            {
+                Console.WriteLine("pasajeros descendiendo");
+            }
+        }
+    }
+}
